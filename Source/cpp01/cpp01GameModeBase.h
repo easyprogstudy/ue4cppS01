8ff3ec8e1966e692482ace1f4ecba3f81180f66e
easyprogstudy <leokiminla@gmail.com>
2023-10-15 18:54:15 +0900

    언리얼엔진 4 c++ - 001

---
+++ Source/cpp01/cpp01GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "cpp01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP01_API Acpp01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
